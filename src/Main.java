67089d352604fd3bef6ae94369bdf45c51efa2a5
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-20 10:23:53 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("1. Stack of integers");
+        GenericStackClient.stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        GenericStackClient.stackOfIStrings();
+    }
+}
